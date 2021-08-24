d16755c74b0008cc3d58bc5acc1963a686c577a0
anuradhajune <anuradhasansi@gmail.com>
2021-08-24 14:52:17 +0530

    i have added java file

---
+++ src/org/phone/Phone.java
@@ -0,0 +1,23 @@
+package org.phone;
+
+public class Phone {
+	private void phoneInfo() {
+System.out.println("Brand : Nokia" );
+	}
+private void phoneInfo(long imei,String make) {
+System.out.println("IMEI :"+ imei);
+System.out.println("Made in "+ make);
+}
+private void phoneInfo(String email, long phno) {
+System.out.println("Email :"+ email);
+System.out.println("Phonenumber:"+ phno);
+
+}
+public static void main(String[] args) {
+	Phone p=new Phone();
+	p.phoneInfo();
+	p.phoneInfo(123456789456l, "India");
+	p.phoneInfo("nokia@gmail.com", 8945632145l);
+	
+}
+}

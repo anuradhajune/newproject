a1b58c8f29d9a1e9f823d4405ee5a37cd258de86
vishnu5456 <jithvishnu46@gmail.com>
2021-08-25 16:48:17 +0530

    i added git sample

--- src/org/git/GitSample.java
+++ src/org/git/GitSample.java
@@ -1,5 +1,31 @@
 package org.git;
 
+
 public class GitSample {
 
+	public class Phone {
+		private void phoneInfo() {
+	System.out.println("Brand : Nokia" );
+		}
+	private void phoneInfo(long imei,String make) {
+	System.out.println("IMEI :"+ imei);
+	System.out.println("Made in "+ make);
+	}
+	private void phoneInfo(String email, long phno) {
+	System.out.println("Email :"+ email);
+	System.out.println("Phonenumber:"+ phno);
+
+	}
+	public  void main(String[] args) {
+		Phone p=new Phone();
+		p.phoneInfo();
+		p.phoneInfo(123456789456l, "India");
+		p.phoneInfo("nokia@gmail.com", 8945632145l);
+		System.out.println("checking");
+		System.out.println("new changes");
+	}
+
+	}
 }
+
+
